76b155a728e22b77fd47b8d1486eda08ac437aea
Simon T <simtho0624@gmail.com>
2023-02-02 11:41:32 +0100

    Step 1-4 are now done.

--- src/main/java/com/example/StringCalculator.java
+++ src/main/java/com/example/StringCalculator.java
@@ -1,4 +1,40 @@
 package com.example;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class StringCalculator {
+    private String delimiter;
+    private String numbers;
+
+    public StringCalculator(String delimiter, String numbers) {
+        this.delimiter = delimiter;
+        this.numbers = numbers;
+    }
+
+    private  int sum() {
+        return Arrays.stream(numbers.split(delimiter))
+                .mapToInt(Integer::parseInt)
+                .sum();
+    }
+
+    public int add(String ManyNumbers) {
+        if (ManyNumbers.isEmpty())
+            return 0;
+
+        return parseInput(ManyNumbers).sum();
+    }
+    private static StringCalculator parseInput(String ManyNumbers) {
+
+        if (ManyNumbers.startsWith("//")){
+            String[] parts = ManyNumbers.split("\n",2);
+            return new StringCalculator(parts[0].substring(2), parts[1]);
+        }
+        else {
+            return new StringCalculator(",|\n", ManyNumbers);
+        }
+    }
 }
--- src/test/java/com/example/StringCalculatorTest.java
+++ src/test/java/com/example/StringCalculatorTest.java
@@ -1,7 +1,44 @@
 package com.example;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorTest {
+    StringCalculator stringCalculator = new StringCalculator("delimeter","numbers");
+    @Test
+    void whenAStringHaveNothingThanIGetBack0() {
+        var result = stringCalculator.add("");
+
+        assertEquals(0, result);
+    }
+    @Test
+    void mathIsEasy3Plus5Is8() {
+
+        assertThat(stringCalculator.add("3,5")).isEqualTo(8);
+    }
+
+
+    @Test
+    void worksWithfourNumbersAndGetTheCorrectSum() {
+        assertThat(stringCalculator.add("7,6,3,1")).isEqualTo(17);
+    }
+
+    @Test
+    void shouldWorkWithANewLine() {
+        assertThat(stringCalculator.add("9\n5")).isEqualTo(14);
+    }
+
+    @Test
+    void splitMoreThanTwoNumbersAndReturnSum() {
+        assertThat(stringCalculator.add("3,2\n5")).isEqualTo(10);
+    }
+
+    @Test
+    void usesDelimiterSpec(){
+        assertThat(stringCalculator.add("//;\n1;2")).isEqualTo(3);
+    }
+
 
 }
